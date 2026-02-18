b75cb82e9a541707e9d39ed53cb9aee92635457a
toomuchtearz <toomuchtearz@gmail.com>
2026-02-18 23:25:56 +0200

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone_validator = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+    )
+    phone_number = models.CharField(
+        validators=[phone_validator,],
+        max_length=17,
+    )
+
+    telegram_nickname = models.CharField(max_length=64, blank=True, null=True)
