f4a8494b026e35778d7c30c97f52d257ececae92
toomuchtearz <toomuchtearz@gmail.com>
2026-02-18 23:35:14 +0200

    add users app to installed apps

--- hold_the_paw/settings.py
+++ hold_the_paw/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "users",
     "pets",
     "rest_framework",
 ]
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+AUTH_USER_MODEL = "users.User"
