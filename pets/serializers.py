b209655c6ab7e216904f614117e8a59159ce8660
toomuchtearz <toomuchtearz@gmail.com>
2026-02-18 21:35:51 +0200

    create AdSerializer

--- pets/serializers.py
+++ pets/serializers.py
@@ -9,3 +9,23 @@ class PetSerializer(serializers.ModelSerializer):
         model = Pet
         fields = "__all__"
 
+class AdSerializer(serializers.ModelSerializer):
+    pet = PetSerializer()
+
+    class Meta:
+        model = Ad
+        fields = (
+            "id",
+            "created_at",
+            "pet"
+        )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            pet_info = validated_data.pop("pet")
+            new_pet = Pet.objects.create(**pet_info)
+            new_ad = Ad.objects.create(
+                **validated_data,
+                pet=new_pet
+            )
+            return new_ad
