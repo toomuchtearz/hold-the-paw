43354957773455bbbb0739bb6e18ddc5e1b7f6bc
toomuchtearz <toomuchtearz@gmail.com>
2026-02-19 15:11:20 +0200

    update serializer according to new model

--- pets/serializers.py
+++ pets/serializers.py
@@ -1,31 +1,9 @@
-from django.db import transaction
 from rest_framework import serializers
 
-from pets.models import Ad, Pet
+from pets.models import PetListing
 
 
-class PetSerializer(serializers.ModelSerializer):
+class PetListingSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Pet
+        model = PetListing
         fields = "__all__"
-
-class AdSerializer(serializers.ModelSerializer):
-    pet = PetSerializer()
-
-    class Meta:
-        model = Ad
-        fields = (
-            "id",
-            "created_at",
-            "pet"
-        )
-
-    def create(self, validated_data):
-        with transaction.atomic():
-            pet_info = validated_data.pop("pet")
-            new_pet = Pet.objects.create(**pet_info)
-            new_ad = Ad.objects.create(
-                **validated_data,
-                pet=new_pet
-            )
-            return new_ad
