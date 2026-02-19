dc85fc77be6baf37a2406aabf8f6910760a8712d
toomuchtearz <toomuchtearz@gmail.com>
2026-02-19 20:53:05 +0200

    update user models & change username to email

--- users/models.py
+++ users/models.py
@@ -1,7 +1,51 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.translation import gettext as _
+
+
+class UserManager(DjangoUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user_object(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        return user
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -10,6 +54,12 @@ class User(AbstractUser):
         PERSONAL = "personal", "Personal account"
         SHELTER = "shelter", "Shelter"
 
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     phone_validator = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
@@ -17,6 +67,7 @@ class User(AbstractUser):
     phone_number = models.CharField(
         validators=[phone_validator,],
         max_length=17,
+        unique=True,
     )
     telegram_nickname = models.CharField(max_length=64, blank=True, null=True)
 
@@ -26,13 +77,15 @@ class User(AbstractUser):
         max_length=20,
     )
 
+    objects = UserManager()
+
 
 class ShelterProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="shelter_profile")
 
     company_name = models.CharField(max_length=255)
-    address = models.TextField()
-    description = models.TextField(blank=True)
+    address = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
 
 class PersonalProfile(models.Model):
