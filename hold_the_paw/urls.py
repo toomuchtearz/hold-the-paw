9db9e5f95d9bbd1d919b1f2c5a5eff9cb9febf13
toomuchtearz <toomuchtearz@gmail.com>
2026-02-19 14:58:27 +0200

    register users urls in root urls file

--- hold_the_paw/urls.py
+++ hold_the_paw/urls.py
@@ -22,6 +22,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("pets.urls", namespace="pets")),
+    path("api/v1/users/", include("users.urls", namespace="users")),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
