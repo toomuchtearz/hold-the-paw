1ae911385b415276c1c80bd8fc1c5a53019d30ed
toomuchtearz <toomuchtearz@gmail.com>
2026-02-19 20:51:33 +0200

    define get_serializer_class for PetListingViewSet

--- pets/views.py
+++ pets/views.py
@@ -2,10 +2,17 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from pets.models import PetListing
-from pets.serializers import PetListingSerializer
+from pets.serializers import PetListingListSerializer, PetListingRetrieveSerializer
 
 
 class PetListingViewSet(viewsets.ModelViewSet):
     queryset = PetListing.objects.all()
-    serializer_class = PetListingSerializer
+    serializer_class = PetListingListSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+        if self.action == "retrieve":
+            serializer = PetListingRetrieveSerializer
+
+        return serializer
