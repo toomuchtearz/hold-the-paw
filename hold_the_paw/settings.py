e08d9ede085657336155daa7b86f548315efc3b1
toomuchtearz <toomuchtearz@gmail.com>
2026-02-19 14:57:08 +0200

    add SIMPLE_JWT settings

--- hold_the_paw/settings.py
+++ hold_the_paw/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +136,9 @@ REST_FRAMEWORK = {
 
 MEDIA_ROOT = "/files/media"
 MEDIA_URL = "/media/"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=3),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+}
