66a15ed302d63c4389727ca95b18427cee7f4c4f
toomuchtearz <toomuchtearz@gmail.com>
2026-02-26 23:22:31 +0200

    add separaTe register & password change urls

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from users.views import CreateUserView, ManageUserView, RequestPasswordReset, ResetPassword
+from users.views import CreateUserView, ManageUserView, RequestPasswordReset, ResetPassword, ChangePasswordView, CreateShelterView
 
 app_name = "users"
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("register/personal/", CreateUserView.as_view(), name="create-personal"),
+    path("register/shelter/", CreateShelterView.as_view(), name="create-shelter"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path("password-reset/", RequestPasswordReset.as_view(), name="password-reset"),
-    path('password-reset-confirm/<str:token>/', ResetPassword.as_view(), name='password-reset-confirm')
+    path("password-change/", ChangePasswordView.as_view(), name="password-change"),
+    path('password-reset-confirm/<str:token>/', ResetPassword.as_view(), name='password-reset-confirm'),
 ]
