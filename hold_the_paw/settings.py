21a058028b67975afc5650928b7772ef5d216d78
toomuchtearz <toomuchtearz@gmail.com>
2026-02-26 23:19:09 +0200

    add localization

--- hold_the_paw/settings.py
+++ hold_the_paw/settings.py
@@ -114,12 +114,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "uk"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kyiv"
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
