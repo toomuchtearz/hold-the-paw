5ecd620fc985b916e73a88d854011fb138b41689
toomuchtearz <toomuchtearz@gmail.com>
2026-02-28 18:14:09 +0200

    update PetListing view

--- pets/views.py
+++ pets/views.py
@@ -2,17 +2,22 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from pets.models import PetListing
-from pets.serializers import PetListingListSerializer, PetListingRetrieveSerializer
+from pets.serializers import PetListingListSerializer, PetListingRetrieveSerializer, PetListingCreateSerializer
 
 
 class PetListingViewSet(viewsets.ModelViewSet):
     queryset = PetListing.objects.all()
-    serializer_class = PetListingListSerializer
+    serializer_class =  PetListingCreateSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_class(self):
         serializer = self.serializer_class
         if self.action == "retrieve":
             serializer = PetListingRetrieveSerializer
+        elif self.action == "list":
+            serializer = PetListingListSerializer
 
         return serializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
