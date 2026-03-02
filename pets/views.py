767ee1b1e582dbfc16d0078b184154fa91ce496c
toomuchtearz <toomuchtearz@gmail.com>
2026-03-02 04:31:29 +0200

    define filter_backends,ordering_fields,search_fields & filterset_fields in viewset

--- pets/views.py
+++ pets/views.py
@@ -1,5 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters
 
 from pets.models import PetListing
 from pets.serializers import PetListingListSerializer, PetListingRetrieveSerializer, PetListingCreateSerializer
@@ -9,7 +11,28 @@ class PetListingViewSet(viewsets.ModelViewSet):
     queryset = PetListing.objects.all()
     serializer_class =  PetListingCreateSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+    ordering_fields = ("created_at",)
+    search_fields = ("name", "location")
 
+    filterset_fields = (
+        "id",
+        "breed",
+        "pet_type",
+        "age",
+        "gender",
+        "special_needs",
+        "has_passport",
+        "is_vaccinated",
+        "is_sterilized",
+        "color",
+        "status",
+
+    )
     def get_serializer_class(self):
         serializer = self.serializer_class
         if self.action == "retrieve":
